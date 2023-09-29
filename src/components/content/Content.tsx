37b36d1ccbc19c5032344c40a18cd62144d30509
the22ndoctober <the22thoctober@gmail.com>
2023-09-29 18:17:26 +0300

    small add

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -6,20 +6,23 @@ import Grid from '@mui/material/Grid'
 import CartItem from './CartItem/CartItem'
 import Typography from '@mui/material/Typography'
 
+
 const Content = () => {
 
     const items = useSelector(selectState)
     const dispatch = useDispatch()
 
   return (
-    <Box sx={{p:3, height: 700,width:1000, display:'flex',justifyContent:'center', overflowY:'scroll'}}>
-        <Box sx={{rowGap:1, display:'flex', flexDirection:'column'}}>
-
-        <Grid container spacing={0} direction={'row'} columnGap={2} px={1}>
+    <Box sx={{p:3, width:700, display:'flex',justifyContent:'center', flexDirection:'column', m:'0 auto' }}>
+        
+        <Grid container spacing={0} direction={'row'} columnGap={2} p={1} sx={{width:550}}>
             <Typography variant="body1" color="initial">№</Typography>
             <Typography variant="body1" color="initial">Назва</Typography>
             <Typography variant="body1" sx={{ml:'auto'}} color="initial">Ячейка</Typography>
         </Grid>
+        <Box sx={{width: '100%',rowGap:1, display:'flex', flexDirection:'column', height: 700, overflowY:'scroll'}}>
+
+        
         {items.map((item:any, id: number)=>{
             return <CartItem
                 id={id}
